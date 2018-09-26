9f38c9e59f45c2c6533df7ff3cc5f4a1b6268e8e
SergioGeek <sergio.it.geek@gmail.com>
2018-09-26 20:17:43 +0200

    sin avances

--- modules/loader.py
+++ modules/loader.py
@@ -3,6 +3,7 @@ from os import listdir
 
 import numpy
 
+
 class Loader:
 
 
@@ -11,6 +12,8 @@ class Loader:
 
 		self.rute = rute
 
+		self.size = 0
+
 		self.excanges = []
 		self.distances = []
 
@@ -18,13 +21,34 @@ class Loader:
 	#Loading the data
 	def load( self ):
 
+		size_counter = 0
 
 		for arch in listdir( self.rute ):
 			
 			if( arch[6] == "d" ):
+				print("Nuevo archivoooooooooo")
+
+				for idx, line in enumerate(open( self.rute + "/" + arch, "r" )):
+
+					cleaned_line = line.split()
+
+					if( len(cleaned_line) != 0 ):
+				
+						if( idx == 0 ):
+						
+							self.size = int( cleaned_line[0] )
+							matrix = numpy.empty(( self.size, self.size ))
+
+					
+						else:
+							print(len(cleaned_line))
+							size_counter += len( cleaned_line )
+						
+						
+						
+						
+
+				size_counter = 0
 
 
-				pfile = open( self.rute + "/" + arch, "r" )
-
-
-			
+		#break
