c276b93e53e8a7dbdd08a6f1a49fcec299d25c70
SergioGeek <sergio.it.geek@gmail.com>
2018-09-27 23:36:49 +0200

    sin terminar; no se contemplaba cuando los tamaños eran desiguales por arriba; se desechaban valores

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ if __name__ == "__main__":
 	prueba = Loader( "/home/anonymous/Desktop/datos" )
 
 	prueba.load()
+
+
+	print( prueba.exchanges[0] )
--- modules/loader.py
+++ modules/loader.py
@@ -14,7 +14,7 @@ class Loader:
 
 		self.size = 0
 
-		self.excanges = []
+		self.exchanges = []
 		self.distances = []
 
 
@@ -22,33 +22,67 @@ class Loader:
 	def load( self ):
 
 		size_counter = 0
+		
 
 		for arch in listdir( self.rute ):
-			
-			if( arch[6] == "d" ):
-				print("Nuevo archivoooooooooo")
 
+			
+
+			#Diferencio si es un archivo .dat o un .sln dentro de la carpeta			
+			if( arch[4] == "1" and arch[6] == "d" ):
+
+				row_pos = 0 #Posición de inserción de la fila
+				col_pos = 0 #Posición de inserción de la columna
+
+				print( arch )
+				change_mtx_type = False
+
+				#Recorro línea a línea el fichero abierto
 				for idx, line in enumerate(open( self.rute + "/" + arch, "r" )):
 
-					cleaned_line = line.split()
 
+					#Limpio la línea de espacios en blanco quedándome con los datos
+					cleaned_line = line.split()
+					
+					#Verificamos que la línea tenga datos
 					if( len(cleaned_line) != 0 ):
-				
+
+						#Si estamos en la primera línea (primera iteración) nos quedamos con el tamaño y creamos la matriz
 						if( idx == 0 ):
 						
 							self.size = int( cleaned_line[0] )
 							matrix = numpy.empty(( self.size, self.size ))
-
-					
+				
 						else:
-							print(len(cleaned_line))
-							size_counter += len( cleaned_line )
-						
-						
-						
-						
+							
+							if( col_pos == self.size ):
+	
+								if ( row_pos < self.size ):
+									
+									row_pos += 1
+									col_pos = 0
+									
 
-				size_counter = 0
+	
+								else:
+									print( "Termino de matriz--------------------------")
+									if ( change_mtx_type == False ):
 
+										self.exchanges.append( matrix )
+										change_mtx_type = True
+										row_pos += 0
 
-		#break
+									else:
+										
+										self.distances.append( matrix)
+									
+									matrix = numpy.empty(( self.size, self.size ))
+
+							else:
+								
+								for dinx in cleaned_line:
+								
+									matrix[row_pos][col_pos] = dinx
+									col_pos += 1
+							
+
