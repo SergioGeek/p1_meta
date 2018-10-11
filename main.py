9158d4d03938ab5d99fa3538c2151e3041c47e5c
SergioGeek <sergio.it.geek@gmail.com>
2018-10-11 16:31:50 +0200

    carga de ficheros de solución y cambio de estructuras (listas -> diccionarios)

--- main.py
+++ main.py
@@ -10,11 +10,3 @@ if __name__ == "__main__":
 
 	prueba.load()
 	
-	for idx, df in enumerate( prueba.dfile_names ):
-
-		print( "Nombre del fichero: " )
-		print( prueba.dfile_names[idx] )
-		print("Mostramos los intercambios" )
-		print( prueba.exchanges[idx] )
-		print("Mostramos las distancias" )
-		print( prueba.distances[idx] )
--- modules/loader.py
+++ modules/loader.py
@@ -13,9 +13,10 @@ class Loader:
 		self.size = 0
 		
 		#Atributos para los ficheros de datos
-		self.dfile_names =[] #Lista para guardar los nombres de los ficheros
-		self.exchanges = [] #Lista en la que se guardan las matrizes con la información de los intercambios
-		self.distances = [] #Lista en la que se guardan las matrizes con la información de las distancias
+		self.dfile_exchange = {} #Diccionario que por nombre de archivo guarda su solución
+		self.dfile_distance = {} #Diccionario que por nombre de archivo guarda su solución
+		self.sfile_solution = {} #Diccionario que por nombre de archivo guarda su solución
+		self.sfile_cost = {} #Diccionario que por nombre de archivo guarda
 
 
 	#Lectura de datos
@@ -24,9 +25,7 @@ class Loader:
 		#Leemos todos los datos de la ruta que se pasa
 		for arch in listdir( self.rute ):
 			
-			if( arch[6] == "d" ): #Para saber si es un .dat y no un .sln
-
-				self.dfile_names.append( arch ) #Añadimos el nombre del fichero
+			if( arch[6] == "d" ): #Para saber si es un .dat
 
 				row_pos = 0 #Posición de inserción de la fila
 				col_pos = 0 #Posición de inserción de la columna
@@ -39,7 +38,7 @@ class Loader:
 
 				#Sacamos de la lista la primera posición y la guardamos en el tamaño de la matriz que crearemos
 				self.size = int(data.pop(0))
-				#Matriz en la que se almacenarán los datos y se guardará en la lista correspodiente
+				#Matriz en la que se almacenarán los datos y se guardará en diccionario correspodiente
 				matrix = numpy.empty( (self.size, self.size), int )
 
 				for itm in data: #Se recorren todos los valore
@@ -57,7 +56,7 @@ class Loader:
 
 							if( change_mtx_type == False): #Si la matriz no se ha cambiado...
 								
-								self.exchanges.append( matrix ) #Metemos la matriz de intercambios en su estructura,
+								self.dfile_exchange[arch[0:5]] = matrix #Metemos la matriz de intercambios en su 														#estructura,
 								matrix = numpy.empty( (self.size, self.size), int ) # limpiamos la matriz
 								row_pos = 0
 								change_mtx_type = True
@@ -65,11 +64,22 @@ class Loader:
 
 							else:
 					
-								self.distances.append( matrix ) #Metemos la matriz de distancias en su estructura
+								self.dfile_distance[arch[0:5]] = matrix #Metemos la matriz de distancias en 														#su estructura
 
-								
+			
+			if( arch[6] == "s" ): #Para saber si es un .sln
+
+				sfile = open( self.rute + "/" + arch, "r" ) #Se habre el fichero
+
+				sln = sfile.read().split() #Se eliminan todos los espacios del fichero
+
+				sln.pop(0) #Saco el tamaño que no interesa
+
+				self.sfile_cost[arch[0:5]] = sln.pop(0)
+
+				self.sfile_solution[arch[0:5]] = sln
+
 
 				
 
-
-			
+				
